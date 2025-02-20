23bea3fd10792d828210289003ec26f2626c0e36
seejl25 <seejunlong@u.nus.edu>
2025-02-20 23:29:59 +0800

    Create new page for clicking sign up button Added: - register form after clicking button - input details for form - styling using css - javascript to alert users form submitted

---
+++ form.js
@@ -0,0 +1,7 @@
+function submitFunction() {
+    alert("Thank you for your submission! We will contact you within 3 working days.")
+}
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", submitFunction);
+
